651f63ca15fb3f401423977a0aea1cab63aa4cb5
kondziet <konrad.dabrowski54@gmail.com>
2023-02-13 17:31:39 +0100

    Add book display functionality

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const submitBtn = document.querySelector(".submit-btn")
-const 
+window.onload = displayBooks;
+
+const submitBtn = document.querySelector(".submit-btn");
+const bookContainer = document.querySelector(".book-container");
 
 let books = [
     new Book("Fablehaven", "Brandon Mull", 323),
@@ -10,6 +12,32 @@ function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+}
+
+function displayBooks() {
+    while(bookContainer.firstChild) {
+        bookContainer.firstChild.remove();
+    }
+    
+    for(let book of books) {
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+
+        const author = document.createElement("p");
+        author.textContent = book.author;
+
+        const title = document.createElement("p");
+        title.textContent = book.title;
+
+        const pages = document.createElement("p");
+        pages.textContent = book.pages;
+
+        bookCard.appendChild(author);
+        bookCard.appendChild(title);
+        bookCard.appendChild(pages);
+
+        bookContainer.appendChild(bookCard);
+    }
 }
 
 function addBook() {
@@ -29,4 +57,5 @@ function addBook() {
 submitBtn.addEventListener("click", e => {
     e.preventDefault();
     addBook();
+    displayBooks();
 })
