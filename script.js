05d855f02ec0b28a8dfcd61d8a5cb6f15fc0b644
kondziet <konrad.dabrowski54@gmail.com>
2023-02-13 18:09:11 +0100

    Add book removal functionality

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ window.onload = displayBooks;
 const bookForm = document.querySelector(".book-form");
 const bookContainer = document.querySelector(".book-container");
 
+let index = 0;
+
 let books = [
     new Book("Fablehaven", "Brandon Mull", 323),
     new Book("Like a Virgin", "Richard Branson", 248)
 ];
 
 function Book(title, author, pages) {
+    this.index = index++;
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -22,6 +25,7 @@ function displayBooks() {
     for(let book of books) {
         const bookCard = document.createElement("div");
         bookCard.classList.add("book-card");
+        bookCard.setAttribute("data-key", book.index);
 
         const author = document.createElement("p");
         author.textContent = book.author;
@@ -32,9 +36,22 @@ function displayBooks() {
         const pages = document.createElement("p");
         pages.textContent = book.pages;
 
+        const removeBtn = document.createElement("button");
+        removeBtn.addEventListener("click", e => {
+            const bookCard = e.target.parentElement;
+            const bookIndex = bookCard.getAttribute("data-key");
+
+            books = books.filter(book => book.index != bookIndex);
+
+            bookCard.remove();
+        })
+        removeBtn.textContent = "Remove";
+
+
         bookCard.appendChild(author);
         bookCard.appendChild(title);
         bookCard.appendChild(pages);
+        bookCard.appendChild(removeBtn);
 
         bookContainer.appendChild(bookCard);
     }
