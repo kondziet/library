d5d5d022210f655a091712ee51a9adcdba43ca13
kondziet <konrad.dabrowski54@gmail.com>
2023-02-13 16:43:07 +0100

    Create function for adding single book

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const submitBtn = document.querySelector(".submit-btn")
+const 
+
 let books = [
     new Book("Fablehaven", "Brandon Mull", 323),
     new Book("Like a Virgin", "Richard Branson", 248)
@@ -8,3 +11,22 @@ function Book(title, author, pages) {
     this.author = author;
     this.pages = pages;
 }
+
+function addBook() {
+    const author = document.querySelector("#author");
+    const title = document.querySelector("#title");
+    const pages = document.querySelector("#pages");
+    
+    books.push(
+        new Book(author.value, title.value, pages.value)
+    );
+
+    author.value = "";
+    title.value = "";
+    pages.value = "";
+}
+
+submitBtn.addEventListener("click", e => {
+    e.preventDefault();
+    addBook();
+})
